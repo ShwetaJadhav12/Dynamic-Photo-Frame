4231fd18d8be30ac40409b70e8d09b82202c06a3
ShwetaJadhav12 <shwetapjadhav12@gmail.com>
2024-12-05 22:44:23 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/photoframe/MainActivity.kt
+++ app/src/main/java/com/example/photoframe/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var image:ImageView
-    var names= arrayOf("MS Dhoni","Virat Kohli","Rohit Sharma","Gautam Gambhir")
+    var names= arrayOf("MS Dhoni","Virat Kohli","Rohit Sharma","Rahul Dravid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
